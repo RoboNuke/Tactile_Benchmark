19a0b2abe4c8a6789b2ee58112533ad54166001c
RoboNuke <brownhun@oregonstate.edu>
2024-09-12 16:13:20 -0700

    Barnicle scrap is passing all tests and ready for deployment!

--- tasks/barnicle_scrap.py
+++ tasks/barnicle_scrap.py
@@ -67,7 +67,9 @@ class ScrapBarnicleEnv(BaseEnv):
     NUM_BARNICLES: int = 4
     BARNICLE_HEIGHT_FRAC: torch.Tensor = torch.Tensor([0.1, 0.30, 0.30, 0.30])
 
-    FORCE_DMG_TABLE: float
+    table_top: Actor
+    FORCE_DMG_TABLE: float = 10.0
+    TABLE_TOP_THICKNESS: float = 0.01
     def __init__(self, *args, 
                  robot_uids="panda", 
                  dmg_table_force = 25.0,  
@@ -88,7 +90,7 @@ class ScrapBarnicleEnv(BaseEnv):
         self.FORCE_DMG_TABLE = dmg_table_force
         self.BARNICLE_FRICTION = barnicle_friction
 
-        self.max_table_force = torch.zeros(self.num_envs)
+        self.max_table_force = torch.zeros(self.num_envs, dtype=torch.float32, device=self.device)
     
 
     @property
@@ -103,6 +105,7 @@ class ScrapBarnicleEnv(BaseEnv):
 
     
     def _load_scene(self, options: dict):
+        self.barnicles = []
         self.table_scene = TableSceneBuilder(self)
         self.table_scene.build()
 
@@ -163,8 +166,8 @@ class ScrapBarnicleEnv(BaseEnv):
                 builder = self.scene.create_actor_builder()
                 builder.add_box_collision(
                     half_size=half_sizes[i,:],
-                    material=None, #physical_material,
-                    density=ds[i]#*(1-self.BARNICLE_HEIGHT_FRAC[i])
+                    material=physical_material,
+                    density=ds[i]/self.BARNICLE_HEIGHT_FRAC[i] if  i == 0 else ds[i]
                 )
 
                 builder.add_box_visual(
@@ -176,12 +179,49 @@ class ScrapBarnicleEnv(BaseEnv):
                 builder.set_scene_idxs(scene_idxs)
                 barn = builder.build(name=f"barnicle_{barn_idx}_{i}")
                 #print(barn.angular_damping, barn.linear_damping)
-                barn.set_angular_damping(10.0)
+                #barn.set_angular_damping(10.0)
                 #barn.set_linear_damping(10.0)
                 #print(barn.angular_damping, barn.linear_damping)
 
                 new_barns.append(barn)
             self.barnicles.append(Actor.merge(new_barns, f"barnicle_{barn_idx}"))
+        self.barn_starts = torch.zeros((self.num_envs,2), dtype=torch.float32, device=self.device)
+
+        # add tabletop layer for force collisions
+
+        half_sizes = torch.zeros((3))
+        half_sizes[0] = 6 * self.BARNICLE_RADIUS_DELTA
+        half_sizes[1] = 6 * self.BARNICLE_RADIUS_DELTA
+        half_sizes[2] = self.TABLE_TOP_THICKNESS/2
+        physical_material: sapien.PhysicalMaterial = self.scene.sub_scenes[0].create_physical_material(
+            static_friction=fs[i]/(1+barn_idx),
+            dynamic_friction=fs[i]/(1+barn_idx),
+            restitution=0.5,
+        )
+        table_tops = []
+        for i in range(self.num_envs):
+            scene_idxs = [i]
+            builder = self.scene.create_actor_builder()
+            builder.add_box_collision(
+                half_size=half_sizes,
+                material=None, #physical_material,
+                density=10*self.BARNICLE_DENSITY
+            )
+
+            builder.add_box_visual(
+                half_size=half_sizes,
+                material=sapien.render.RenderMaterial(
+                    base_color=[1,1,1,1],
+                )
+            )
+
+            builder.set_scene_idxs(scene_idxs)
+            table_tops.append(builder.build_kinematic(name=f"table_top_{i}"))
+        self.table_top = Actor.merge(table_tops, name='table_top')
+        self.max_table_force = torch.zeros((self.num_envs), 
+                                           dtype=torch.float32, 
+                                           device=self.device
+        )
         
 
     def _initialize_episode(self, env_idx: torch.Tensor, options: dict):
@@ -210,6 +250,13 @@ class ScrapBarnicleEnv(BaseEnv):
             self.agent.robot.set_qpos(qpos)
             self.agent.robot.set_pose(sapien.Pose([-0.615, 0, 0]))
 
+            # set tabletop
+            ttpos = torch.zeros((b,3), dtype=torch.float32, device=self.device)
+            ttqs = torch.zeros((b,4), dtype=torch.float32, device=self.device)
+            ttpos[:,2] = self.TABLE_TOP_THICKNESS / 2.0
+            ttqs[:,3] = 1.0
+            self.table_top.set_pose(Pose.create_from_pq(ttpos, ttqs))
+
             # initialize the barnicles
             xy = randomization.uniform(
                 low=torch.tensor(self.BARNICLE_SPAWN_CENTER) - self.BARNICLE_SPAWN_DELTA, 
@@ -218,7 +265,7 @@ class ScrapBarnicleEnv(BaseEnv):
             )
             pos = torch.zeros((b,3), dtype=torch.float32, device=self.device)
             pos[:,:2] = xy
-
+            self.barn_starts[env_idx] = xy
             quat = randomization.random_quaternions(
                 b,
                 self.device,
@@ -230,10 +277,14 @@ class ScrapBarnicleEnv(BaseEnv):
             self.barnicle_hs = self.barnicle_hs.to(device=self.device)
             
             for i, barns in enumerate(self.barnicles):
-                print(i)
-                pos[:,2] = h_offsets + self.barnicle_hs * self.BARNICLE_HEIGHT_FRAC[i]/2.0
+                pos[:,2] = (h_offsets + self.barnicle_hs[env_idx] * 
+                            self.BARNICLE_HEIGHT_FRAC[i]/2.0 +
+                            0.001 + self.TABLE_TOP_THICKNESS )
                 barns.set_pose(Pose.create_from_pq(pos, quat))
-                h_offsets += self.barnicle_hs * self.BARNICLE_HEIGHT_FRAC[i]
+                h_offsets += self.barnicle_hs[env_idx] * self.BARNICLE_HEIGHT_FRAC[i]
+
+            self.max_table_force[env_idx] *= 0# torch.zeros((b), dtype=torch.float32, device=self.device)
+
 
     def evaluate(self):
         """ Returns info with:
@@ -246,13 +297,36 @@ class ScrapBarnicleEnv(BaseEnv):
         """
         is_robot_static = self.agent.is_static(0.2)
 
+        # get current force on table
+        table_force = torch.linalg.norm(
+            self.scene.get_pairwise_contact_forces(self.table_top, self.agent.tcp)
+        )
+        
+        # get max force on table this episode
+        self.max_table_force = torch.maximum(table_force, self.max_table_force)
+
+        # get barnicle still attached
+        attached_barnicles = torch.zeros((self.num_envs), dtype=torch.int32, device=self.device)
+        for barn in self.barnicles:
+            attached_barnicles += torch.linalg.norm(
+                barn.pose.p[:,:2] - self.barn_starts, axis=1
+                ) < 0.001
+        # determine success
+        succ = torch.zeros((self.num_envs), dtype=torch.bool, device = self.device)
+        succ[self.max_table_force < self.FORCE_DMG_TABLE] = True
+        #succ[~is_robot_static] = False
+        succ[attached_barnicles > 0] = False
+        
+        # determine failure
+        fail = self.max_table_force >= self.FORCE_DMG_TABLE
+        
         return {
-            "fail": torch.zeros(self.num_envs),
-            "success": torch.ones(self.num_envs),
-            'attached_barnicles':  torch.zeros(self.num_envs),
+            "fail": fail,
+            "success": succ,
+            'attached_barnicles':  attached_barnicles,
             "is_robot_static": is_robot_static,
-            "max_table_force": torch.zeros(self.num_envs),
-            "table_force":  torch.zeros(self.num_envs)
+            "max_table_force": self.max_table_force,
+            "table_force": table_force
         }
 
     def get_obs(self, info: Optional[Dict]=None):
@@ -296,19 +370,41 @@ class ScrapBarnicleEnv(BaseEnv):
     def _get_obs_extra(self, info: Dict):
         """ Adds force-torque if no_ft not in obs_mode """
         data = dict()
+        if "state" in self.obs_mode:
+            # barnicle (base) xyz
+            # tcp_pose
+            pass ###################################################
         if self.return_force_data:
-            data['force'] = torch.zeros((self.num_envs, 3), device=self.device)
+            data['force'] = self.agent.tcp.get_net_contact_forces()
         return data
 
     def compute_dense_reward(self, obs: Any, action: torch.Tensor, info: Dict):
         """ Dense reward has following elements:
             - reaching (dist to barnicle) max: 0-1
             - number of barnicles attached max: 0-1
-            - if moving while no barnicles attached max: 0-1
             - current force on table: max: 0-1 1 => no force 0 => >max force
             total max reward: 4 
         """
-        return torch.ones(self.num_envs) * 4.0
+        r = torch.zeros((self.num_envs), dtype=torch.float32, device=self.device)
+
+        tcp_to_obj_dist = torch.linalg.norm(
+            self.agent.tcp.pose.p- self.barnicles[0].pose.p,
+            axis = 1
+        )
+        r += 1 - torch.tanh(5 * tcp_to_obj_dist)
+
+        # how many barnicles attached
+        #print("attached:", info['attached_barnicles'])
+        r += 1.0 - info['attached_barnicles'] / self.NUM_BARNICLES
+
+        # how much force we are putting out
+        #print("pre-r:", r)
+        #print("table forece:", info['table_force'])
+        r -= 1-torch.sigmoid(14-20*info['table_force'] / self.FORCE_DMG_TABLE)
+        
+        r[info['fail']] = -1
+        r[info['success']] = 3
+        return r
 
     def compute_normalized_dense_reward(self, obs: Any, action: torch.Tensor, info: Dict):
-        return self.compute_dense_reward(obs, action,  info) / 4.0
+        return self.compute_dense_reward(obs, action,  info) / 3.0
--- tests/test_barnicle_scrap.py
+++ tests/test_barnicle_scrap.py
@@ -29,12 +29,12 @@ class TestBarnicleScrap(unittest.TestCase):
         cls.env_kwargs = cls.env_info["env_kwargs"]
         cls.num_envs = 2
         cls.min_con_force: float = 1.0
-        cls.max_reward = 4.0
+        cls.max_reward = 3.0
         cls.has_set = False
     #def setUp(self):
     #    print("Problem setup!")
-    """
-    def test_play(self):
+    
+    """def test_play(self):
         self.set_envs()
         self.envs.reset()
         
@@ -48,7 +48,7 @@ class TestBarnicleScrap(unittest.TestCase):
             i += 1
             if i % 100 == 0:
                 self.envs.reset()
-    """     
+    """
 
     def set_envs(self, reward_mode='sparse', obs_mode='state'):
         self.has_set = True
@@ -116,7 +116,6 @@ class TestBarnicleScrap(unittest.TestCase):
         self.set_envs(obs_mode='state')
         obs_wf, _ = self.envs.reset()
         self.envs.close()
-        print("here)")
         # check without force
         self.set_envs(obs_mode='state_no_ft')
         obs_wof, _ = self.envs.reset()
@@ -141,46 +140,57 @@ class TestBarnicleScrap(unittest.TestCase):
         self.set_envs()
         self.envs.reset()
         # move all barnicles
-        for barn in self.envs.barnicles:
-            cur_pos = barn.pose
-            cur_pos[:,0] += 10.0 # move it farm away
-            barn.pose = cur_pos
+        for barn in self.envs.unwrapped.barnicles:
+            barn.set_pose(sapien.Pose([10,10,0]))
 
         # eval
-        info = self.envs.evaluate()
+        info = self.envs.unwrapped.evaluate()
 
         assert(torch.all(info['success']))
-        assert(torch.all(1-info['fail']))
+        assert(torch.all(~info['fail']))
         assert(torch.all(info['is_robot_static']))
-        assert(torch.all(info['max_table_force'] < self.envs.FORCE_DMG_TABLE))
+        assert(torch.all(info['max_table_force'] < self.envs.unwrapped.FORCE_DMG_TABLE))
 
     def test_evaluate_fail_table_force(self):
         # check failure by max table force exceeded
         self.set_envs()
         self.envs.reset()
         
-        for i in range(149):
-            self.envs.step(self.envs.action_space.sample()*0)
+        starting_poses = self.envs.unwrapped.barn_starts.clone()
+        for i in range(50):
+            if i == 25: # halfway  "fail" in env 1
+                self.envs.unwrapped.max_table_force[0] += self.envs.unwrapped.FORCE_DMG_TABLE * 2
+            if i == 40: # last step "fail" in env 2
+                self.envs.unwrapped.max_table_force[1] += self.envs.unwrapped.FORCE_DMG_TABLE * 2
+            
+            obs, r, term, trunc, info = self.envs.step(self.envs.action_space.sample()*0)
+            #print(term, trunc,  info['fail'])
+            if i == 25:
+                assert(term[0] and not term[1]) # only env 1 failed
+                assert(info['fail'][0] and not info['fail'][1])
+                self.envs.reset(options={'env_idx':[0]})
+                # check env reset correctly
+                assert(self.envs.unwrapped.elapsed_steps[0] == 0)
+                assert(self.envs.unwrapped.elapsed_steps[1] == 26)
+                # ensure barnicles updated correctly
+                assert(torch.linalg.norm(starting_poses[0,:] - self.envs.unwrapped.barn_starts[0,:]) > 0.001)
+                assert(torch.linalg.norm(starting_poses[1,:] - self.envs.unwrapped.barn_starts[1,:]) < 0.0001)
 
-        self.envs.max_table_force += self.envs.FORCE_DMG_TABLE * 2
+            if i == 26:
+                assert(self.envs.unwrapped.elapsed_steps[0] == 1) # only first env reset
+                assert(self.envs.unwrapped.elapsed_steps[1] == 27)
+
+            if i >= 40: # check fail stays until reset
+                assert(info['fail'][1] and not info['fail'][0])
+            elif not i == 25:
+                assert(not info['fail'][1] and not info['fail'][0])
+            
 
         _, _, _, _, info = self.envs.step(self.envs.action_space.sample()*0)
 
-        assert(torch.all(info['fail']))
-
-    def test_evaluate_fail_barnicle(self):
-        # check failure by barnicle still attached
-        self.set_envs()
-        self.envs.reset()
-
-        for i in range(150):
-            self.envs.step(self.envs.action_space.sample()*0)
-        # eval
-        info = self.envs.evaluate()
-
-        assert(torch.all(1-info['success']))
-        assert(torch.all(info['fail']))
-        assert(torch.all(info['attached_barnicles'] > 0 ))
+        assert(info['fail'][1])
+        assert(torch.all(~info['success'][1]))
+        assert(torch.all(info['max_table_force'][1] == self.envs.unwrapped.FORCE_DMG_TABLE * 2))
 
     def test_dense_reward_barnicles(self):
         # ensure reward increases as barnicles are removed
@@ -188,15 +198,13 @@ class TestBarnicleScrap(unittest.TestCase):
         self.envs.reset()
         _, arm_dist_offset, _,_,_ = self.envs.step(self.envs.action_space.sample()*0)
 
-        barn_rewards = [0.06, 0.24, 0.5, 1.0]
-
-        for i in range(len(self.envs.barnicles)-1,-1,-1):
-            barn = self.envs.barnicles[i]
-            cur_pos = barn.pose
-            cur_pos[:,0] += 10.0 # move it farm away
-            barn.pose = cur_pos
-            _, reward, _,_,_ = self.env.step(self.envs.action_space.sample()*0)
-            assert(reward - arm_dist_offset == barn_rewards[i])
+        nb = len(self.envs.unwrapped.barnicles)
+        for i in range(nb):
+            barn = self.envs.unwrapped.barnicles[i]
+            barn.set_pose(sapien.Pose([10,10,0]))
+            _, reward, _,_,_ = self.envs.step(self.envs.action_space.sample()*0)
+            for k in range(self.num_envs):
+                assert(reward[k] - arm_dist_offset[k] - (i+1)/nb < 0.001)
         
 
     def test_normalized_dense_reward_barnicles(self):
@@ -204,42 +212,49 @@ class TestBarnicleScrap(unittest.TestCase):
         self.set_envs(reward_mode='normalized_dense')
         self.envs.reset()
         _, arm_dist_offset, _,_,_ = self.envs.step(self.envs.action_space.sample()*0)
-        arm_dist_offset /= self.max_reward
+        #arm_dist_offset /= self.max_reward
 
-        barn_rewards = [0.06, 0.24, 0.5, 1.0]
-
-        for i in range(len(self.envs.barnicles)-1,-1,-1):
+        nb = len(self.envs.unwrapped.barnicles)
+        for i in range(nb):
             barn = self.envs.barnicles[i]
-            cur_pos = barn.pose
-            cur_pos[:,0] += 10.0 # move it farm away
-            barn.pose = cur_pos
-            _, reward, _,_,_ = self.env.step(self.envs.action_space.sample()*0)
-            assert(reward - arm_dist_offset == barn_rewards[i])
+            barn.set_pose(sapien.Pose([10,10,0]))
+            _, reward, _,_,_ = self.envs.step(self.envs.action_space.sample()*0)
+            for k in range(self.num_envs):
+                assert(reward[k] - arm_dist_offset[k] - (i+1)/nb/self.max_reward < 0.001)
         
 
     def test_dense_reward_force_on_table(self):
         # ensure reward increases as barnicles are removed
-        self.set_envs(reward_mode='normalized_dense')
+        self.set_envs(reward_mode='dense')
         obs = self.envs.reset()
         _, arm_dist_offset, _,_,_ = self.envs.step(self.envs.action_space.sample()*0)
 
+        arm_dist_offset[1] -= 0.25
+
+        #print("No force")
         self.table_force(obs, arm_dist_offset, 0.0, 0.0)
-        self.table_force(obs, arm_dist_offset, 0.5*self.envs.FORCE_DMG_TABLE, -0.5)
-        self.table_force(obs, arm_dist_offset, self.envs.FORCE_DMG_TABLE, -1.0, fail=1)
-        self.table_force(obs, arm_dist_offset, 2*self.envs.FORCE_DMG_TABLE, -1.0, fail=1)
+        #print("half")
+        self.table_force(obs, arm_dist_offset, 0.5*self.envs.unwrapped.FORCE_DMG_TABLE, -0.01799)
+        #print("full")
+        self.table_force(obs, arm_dist_offset, self.envs.unwrapped.FORCE_DMG_TABLE, -0.99753, fail=1)
+        #print("double")
+        self.table_force(obs, arm_dist_offset, 2*self.envs.unwrapped.FORCE_DMG_TABLE, -1.0, fail=1)
         
 
     def table_force(self, obs, arm_offset, table_force, result, fail=0):
         info = {
-            "fail": torch.ones((self.num_envs)) * fail,
-            "success": torch.ones((self.num_envs)) * (1-fail),
-            'attached_barnicles': 0,
-            "is_robot_static": torch.ones((self.num_envs)),
-            "max_table_force": torch.ones((self.num_envs)) * table_force,
-            "table_force": torch.ones((self.num_envs)) * table_force
+            "fail": torch.zeros((self.num_envs), device="cuda:0", dtype=torch.bool),
+            "success": torch.zeros((self.num_envs), device="cuda:0", dtype=torch.bool),
+            'attached_barnicles': torch.ones((self.num_envs), device="cuda:0")*4,
+            "is_robot_static": torch.ones((self.num_envs), device="cuda:0"),
+            "max_table_force": torch.ones((self.num_envs), device="cuda:0") * table_force,
+            "table_force": torch.ones((self.num_envs), device="cuda:0") * table_force
         }
-        r = self.envs.compute_dense_reward(obs, self.envs.action_space.sample()*0, info) - arm_offset
-        nr = self.envs.compute_normalized_dense_reward(obs, self.envs.action_space.sample()*0, info) - arm_offset/self.max_reward
+        #print(self.envs.unwrapped.compute_dense_reward(obs, self.envs.action_space.sample()*0, info), arm_offset)
+        r = self.envs.unwrapped.compute_dense_reward(obs, self.envs.action_space.sample()*0, info) - arm_offset
+        nr = self.envs.unwrapped.compute_normalized_dense_reward(obs, self.envs.action_space.sample()*0, info) - arm_offset/self.max_reward
+        #print(r, result)
+        
         assert( torch.all(r - result < 0.001))
         assert( torch.all(r - result > -0.001))
 
