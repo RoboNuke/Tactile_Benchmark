62a9bd5c022964c727188011c15c900ec44939e9
RoboNuke <brownhun@oregonstate.edu>
2024-10-01 13:06:44 -0700

    Fixing conda env setup

--- learning/3_launch_tmux.bash
+++ learning/3_launch_tmux.bash
@@ -10,15 +10,15 @@ tmux send-keys -t "Fragile_Baseline:0" "bash learning/launch.bash 1 2 $1 $2 $3 $
 
 # Create a new window and run script2.sh
 tmux split-window -v
-tmux send-keys -t 1 "bash learning/launch.bash 3 4 $1 $2 $3 $4; exit" C-m
+tmux send-keys -t 1 "conda activate mani && bash learning/launch.bash 3 4 $1 $2 $3 $4; sleep 60; exit" C-m
 
 # Create another window and run script3.sh
 tmux split-window -v
-tmux send-keys -t 2 "bash learning/launch.bash 5 6 $1 $2 $3 $4; exit" C-m
+tmux send-keys -t 2 "conda activate mani && bash learning/launch.bash 5 6 $1 $2 $3 $4; sleep 60; exit" C-m
 
 # Attach to the session
-#tmux attach -t "Fragile_Baseline"
-while tmux list-sessions | grep -q "Fragile_Baseline"; do
-    sleep 600 # check every 10 minutes
-done
+tmux attach -t "Fragile_Baseline"
+#while tmux list-sessions | grep -q "Fragile_Baseline"; do
+#    sleep 600 # check every 10 minutes
+#done
 #echo "   " $1 $2 $3 $4 complete!
