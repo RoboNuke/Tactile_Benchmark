66ecf3cc8302adc13c39d3af82daa2e91c285e65
RoboNuke <brownhun@oregonstate.edu>
2024-10-07 15:01:05 -0700

    Fixed file path

--- learning/HPC/10_launch_tmux.bash
+++ learning/HPC/10_launch_tmux.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 SESSION_NAME="Fragile_Baseline"
 NUM_WINDOWS=10
-
+gpu_path="/nfs/stak/users/brownhun/hpc-share/Tactile_Benchmark/learning/launch.bash"
 tmux new-session -d -s "$SESSION_NAME"
 
 for i in $(seq 1 $NUM_WINDOWS); do
     tmux new-window -t "$SESSION_NAME":$i -n "Window $i"
     tmux send-keys -t "$SESSION_NAME":$i "conda activate mani && "
-    tmux send-keys -t "$SESSION_NAME":$i "bash learning/launch.bash $i $i " 
+    tmux send-keys -t "$SESSION_NAME":$i "bash $gpu_path $i $i " 
     tmux send-keys -t "$SESSION_NAME":$i "$*"
     tmux send-keys -t "$SESSION_NAME":$i "; exit" C-m
     #tmux send-keys -t "$SESSION_NAME":$i "echo $i $i; sleep 2; exit" C-m
