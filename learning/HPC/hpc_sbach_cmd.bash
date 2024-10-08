85a23197aa5f0afeb082937960da5a59c303ecea
RoboNuke <brownhun@oregonstate.edu>
2024-10-08 16:34:34 -0700

    Updates for sbach test

--- learning/HPC/hpc_sbach_cmd.bash
+++ learning/HPC/hpc_sbach_cmd.bash
@@ -15,7 +15,8 @@
 module load cuda/10.1
 eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
 eval "$(conda activate mani)"
-
+echo $CONDA_DEFAULT_ENV
+echo starting_process
 #hol=nvidia-smi --query-gpu=memory.free --format=csv,noheader
 # run my job (e.g. matlab, python)
 bash learning/HPC/hpc_launch.bash $*
