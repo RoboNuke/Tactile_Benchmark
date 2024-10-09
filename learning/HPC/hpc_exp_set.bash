b1342eb48bade2b8a48920ce16864e30f614e4dd
RoboNuke <brownhun@oregonstate.edu>
2024-10-08 17:24:03 -0700

    Updates for sbach test

--- learning/HPC/hpc_exp_set.bash
+++ learning/HPC/hpc_exp_set.bash
@@ -3,7 +3,7 @@ num_steps=150
 total_timesteps=15000000
 
 obs_types=("state_dict" "state_dict_no_ft") # "rgb" "rgb_no_ft")
-dmg_vals=("500.0") # "100000.0" "100.0" "50.0" "250.0" "100.0" )
+dmg_vals=("100000.0" "100.0" "50.0" "250.0" "100.0" ) # "500.0" 
 control_modes=("pd_joint_delta_pos")
 reward_modes=("normalized_dense")
 force_encodings=("FFN")
@@ -14,7 +14,7 @@ for control_mode in ${control_modes[@]}; do
         for force_encoding in ${force_encodings[@]}; do
             for obs_type in ${obs_types[@]}; do
                 for dmg_val in ${dmg_vals[@]}; do 
-                    for i in $(seq 3 10); do
+                    for i in $(seq 1 10); do
                         echo $obs_type $dmg_val
                         exp_name="FPiH_peg_dmg_${dmg_val}"
                         sbatch learning/HPC/hpc_sbach_cmd.bash  $i $i\
