5a1425cceff4c4aa3822aa7767751cb28895f883
Hunter Brown <brownhun@submit-a.hpc.engr.oregonstate.edu>
2024-10-16 13:17:48 -0700

    Current HPC tests

--- learning/HPC/hpc_exp_set.bash
+++ learning/HPC/hpc_exp_set.bash
@@ -2,8 +2,8 @@ task="FragilePegInsert-v1"
 num_steps=150
 total_timesteps=25000000
 
-obs_types=("state_dict" "state_dict_no_ft") # "rgb" "rgb_no_ft")
-dmg_vals=("500.0" "100.0" "50.0" "250.0" "100000.0") 
+obs_types=("state_dict") # "state_dict_no_ft") # "rgb" "rgb_no_ft")
+dmg_vals=("500.0" "100000") # "100.0" "50.0" "250.0" "100000.0") 
 control_modes=("pd_joint_delta_pos")
 reward_modes=("normalized_dense")
 force_encodings=("FFN")
--- learning/HPC/hpc_sbach_cmd.bash
+++ learning/HPC/hpc_sbach_cmd.bash
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH --array=1-2             # set up the array
-#SBATCH -J ViRLTest			    # name of job
-#SBATCH -A virl-grp	            # name of my sponsored account, e.g. class or research group, NOT ONID!
-#SBATCH -p eecs2				# name of partition or queue
-#SBATCH --time=0-3:30:00        # time limit on job: 2 days, 12 hours, 30 minutes (default 12 hours)
+#SBATCH -J StructTest			    # name of job
+##SBATCH -A #virl-grp	            # name of my sponsored account, e.g. class or research group, NOT ONID!
+#SBATCH -p dgx2				# name of partition or queue
+#SBATCH --time=0-12:00:00        # time limit on job: 2 days, 12 hours, 30 minutes (default 12 hours)
 ##SBATCH -N 1                   # number of nodes (default 1)
 #SBATCH --gres=gpu:1            # number of GPUs to request (default 0)
 #SBATCH --mem=16G               # request 10 gigabytes memory (per node, default depends on node)
 #SBATCH -c 16                    # number of cores/threads per task (default 1)
-#SBATCH -o ../outs/batchTest_%A_%a.out		# name of output file for this submission script
-#SBATCH -e ../outs/batchTest_%A_%a.err		# name of error file for this submission script
+#SBATCH -o ../outs/struct_tests_%A_%a.out		# name of output file for this submission script
+#SBATCH -e ../outs/struct_tests_%A_%a.err		# name of error file for this submission script
 # load any software environment module required for app (e.g. matlab, gcc, cuda)
 
 
@@ -27,5 +27,5 @@ else
     beg_idx=6
     end_idx=10
 fi
-#bash learning/HPC/hpc_launch.bash $beg_idx $end_idx $*
-bash learning/HPC/10_launch_tmux.bash $beg_idx $end_idx $*
+bash learning/HPC/hpc_launch.bash $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_ID $*
+#bash learning/HPC/10_launch_tmux.bash $beg_idx $end_idx $*
