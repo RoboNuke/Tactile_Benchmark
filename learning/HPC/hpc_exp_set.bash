bd7b6d3d4cf5caf5471c0363592beaa892d37df3
RoboNuke <brownhun@oregonstate.edu>
2024-10-23 10:20:11 -0700

    Reduced test configurations, reverted params to old model for fair comparision

--- learning/HPC/hpc_exp_set.bash
+++ learning/HPC/hpc_exp_set.bash
@@ -2,8 +2,8 @@ task="FragilePegInsert-v1"
 num_steps=150
 total_timesteps=50000000
 
-obs_types=("state_dict_no_ft" "state_dict") # "rgb" "rgb_no_ft")
-dmg_vals=("500.0" "100000.0") # "100.0" "50.0" "250.0" "100000.0") 
+obs_types=("state_dict_no_ft") # "state_dict") # "rgb" "rgb_no_ft")
+dmg_vals=("100000.0") # "100.0" "50.0" "250.0" "100000.0")  "500.0" 
 control_modes=("pd_joint_delta_pos")
 reward_modes=("normalized_dense")
 force_encodings=("FFN")
@@ -20,9 +20,9 @@ for control_mode in ${control_modes[@]}; do
             for obs_type in ${obs_types[@]}; do
                 for dmg_val in ${dmg_vals[@]}; do
 		            echo ${dmg_val} ${obs_type}
-		            #for (( i=0; i<5; i++ )); do	
+		            #for (( i=0; i<4; i++ )); do	
                     #echo "    " ${critic_n[$i]} ${critic_l[$i]}
-		                exp_name="InitBetter2_${dmg_val}_${obs_type}"
+		                exp_name="OldParamsBro_"
                         sbatch learning/HPC/hpc_sbach_cmd.bash \
                             $task \
                             $obs_type \
--- learning/HPC/hpc_ppo.py
+++ learning/HPC/hpc_ppo.py
@@ -111,13 +111,13 @@ class Args:
     """for benchmarking purposes we want to reconfigure the eval environment each reset to ensure objects are randomized in some tasks"""
     anneal_lr: bool = False
     """Toggle learning rate annealing for policy and value networks"""
-    gamma: float = 0.99 #0.8
+    gamma: float = 0.8 #0.99 #0.8
     """the discount factor gamma"""
-    gae_lambda: float = 0.99 #0.9
+    gae_lambda: float = 0.9 #0.99 #0.9
     """the lambda for the general advantage estimation"""
     num_minibatches: int = 32
     """the number of mini-batches"""
-    update_epochs: int = 10
+    update_epochs: int = 2
     """the K epochs to update the policy"""
     norm_adv: bool = True
     """Toggles advantages normalization"""
@@ -131,7 +131,7 @@ class Args:
     """coefficient of the value function"""
     max_grad_norm: float = 0.5
     """the maximum norm for the gradient clipping"""
-    target_kl: float = 0.05 #0.2
+    target_kl: float = 0.2 #0.05 #0.2
     """the target KL divergence threshold"""
     reward_scale: float = 1.0
     """Scale the reward by this factor"""
