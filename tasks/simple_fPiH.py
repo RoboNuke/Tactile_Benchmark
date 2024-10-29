a02630f99fd1382c9bd25a38ac9588bf5ef7a168
RoboNuke <brownhun@oregonstate.edu>
2024-10-28 18:30:56 -0700

    Fixed floating block

--- tasks/simple_fPiH.py
+++ tasks/simple_fPiH.py
@@ -367,7 +367,7 @@ class SimpleFragilePiH(BaseEnv):
         
         dist = torch.linalg.norm(self.goal_pose.p - self.peg.pose.p, axis=1)
 
-        reward =  1 - torch.tanh(5 * dist) - (~is_grasped)*1.0
+        reward =  1 - torch.tanh(5 * dist) #- (~is_grasped)*1.0
         reward[info["success"]] = self.max_reward
 
         return reward
